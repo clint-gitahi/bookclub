9eb8ec47b15060ec9a51bbb607f593b83026d4c7
clint-gitahi <clingitahi@gmail.com>
2019-09-24 10:45:18 +0300

    add and update item on db

--- server/server/services/BookService.js
+++ server/server/services/BookService.js
@@ -9,6 +9,31 @@ class BookService {
       throw error;
     }
   }
+
+  static async addBook(newBook) {
+    try {
+      return await database.Book.create(newBook);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async updateBook(id, updateBook) {
+    try {
+      const bookToUpdate = await database.Book.findOne({
+        where: { id: Number(id) }
+      });
+
+      if (bookToUpdate) {
+        await database.Book.update(updateBook, { where: { id: Number(id) } });
+
+        return updateBook;
+      }
+      return null;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
 export default BookService;
